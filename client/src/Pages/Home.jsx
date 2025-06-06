cc6c2c4b7cf59f13b0f7d4e8dec183119f2e099f
ansh soni <anshsoni200002@gmail.com>
2025-06-06 15:05:43 +0530

    create complete home page - with signup and signin

---
+++ client/src/Pages/Home.jsx
@@ -0,0 +1,19 @@
+import ExclusiveOffers from '../components/ExclusiveOffers'
+import FeaturedDestination from '../components/FeaturedDestination'
+import Hero from '../components/Hero'
+import NewsLatter from '../components/NewsLatter'
+import Testimonial from '../components/Testimonial'
+
+function Home() {
+  return (
+    <>
+      <Hero />
+      <FeaturedDestination />
+      <ExclusiveOffers />
+      <Testimonial />
+      <NewsLatter />
+    </>
+  )
+}
+
+export default Home
