6ba711ce36c1b69c59a11d515878dae79120a58d
ansh soni <anshsoni200002@gmail.com>
2025-06-07 13:25:38 +0530

    create allRoom, RoomDetails, MyBooking pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,9 @@ import { Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./Pages/Home"
 import Footer from "./components/Footer";
+import AllRooms from "./Pages/AllRooms";
+import RoomDetails from "./Pages/RoomDetails";
+import MyBookings from "./Pages/MyBookings";
 
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes("owner");
@@ -13,6 +16,9 @@ const App = () => {
       <div className="min-h-[70vh]">
         <Routes>
           <Route path="/" element={<Home />}/>
+          <Route path="/rooms" element={<AllRooms />}/>
+          <Route path="/rooms/:id" element={<RoomDetails />}/>
+          <Route path="/my-bookings" element={<MyBookings />}/>
         </Routes>
       </div>
 
