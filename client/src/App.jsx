6f00a86222e0c40b51c94dd1b8865618fe961386
ansh soni <anshsoni200002@gmail.com>
2025-09-20 16:31:58 +0530

    frontend-completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,13 +5,21 @@ import Footer from "./components/Footer";
 import AllRooms from "./Pages/AllRooms";
 import RoomDetails from "./Pages/RoomDetails";
 import MyBookings from "./Pages/MyBookings";
+import HotelRegistration from "./components/HotelRegistration";
+import Layout from "./Pages/HotelOwner/Layout";
+import Dashboard from "./pages/hotelOwner/Dashboard";
+import ListRoom from "./Pages/hotelOwner/ListRoom";
+import AddRoom from "./Pages/hotelOwner/AddRoom";
 
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes("owner");
+  const isHotelReg = false
 
   return (
     <div>
       {!isOwnerPath && <Navbar />}
+
+      {isHotelReg && <HotelRegistration />}
 
       <div className="min-h-[70vh]">
         <Routes>
@@ -19,6 +27,14 @@ const App = () => {
           <Route path="/rooms" element={<AllRooms />}/>
           <Route path="/rooms/:id" element={<RoomDetails />}/>
           <Route path="/my-bookings" element={<MyBookings />}/>
+
+          {/* owner route  */}
+          <Route path="/owner" element={<Layout />}>
+            <Route index element={<Dashboard />}/>
+            <Route path="add-room" element={<AddRoom />}/>
+            <Route path="list-room" element={<ListRoom />}/>
+          </Route>
+
         </Routes>
       </div>
 
